b745b6e3ef56c2a69e50fc4feb07877e5307aef9
Shane Sniteman <ssniteman@gmail.com>
2014-03-07 09:57:49 -0500

    name-input.val =username

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,22 @@
 
-$(function()){
+$(function(){
 	Parse.initialize("t899K0oegoIaqBofzoUptQ5RKNwvC27ouCYQS2f4", "P2Nux1Ndq7wKVkAKazHnW2WZlQoQVLETVvDUbu3S");
-}
+
+
+window.band = new BandCollection;
+band.fetch()
+
+$('.sign-in').click(function(){
+
+	var username = new Band();
+
+		username.save({
+			name: $('.name-input').val(),
+		
+		})
+
+
+});
+
+});
+
